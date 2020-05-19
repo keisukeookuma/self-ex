7a097a0328ce385fd2bab102d61d5d50661f1a3e
keisukeookuma <kb_kumagki@yahoo.co.jp>
2020-05-19 19:46:56 +0900

    constの修正

--- html/conf/const.php
+++ html/conf/const.php
@@ -4,4 +4,10 @@ define('DB_USER',   'Keisuke Okuma');      // MySQLのユーザ名（マイペ�
 define('DB_PASSWD', 'selfex626');    // MySQLのパスワード（マイページのアカウント情報を参
 define('DB_NAME', 'self-ex'); // MySQLのDB名(このコースではMySQLのユーザ名と同じで
 define('DB_CHARSET', 'SET NAMES utf8');  // MySQLのcharset
-define('DSN', 'mysql:dbname='.DB_NAME.';host=mysql;port=3306;charset=utf8');  // データベースのDSN情報
+// if($_SERVER('SERVER_ADDR') === '118.27.4.157'){
+//     $host = 'localhost';
+// }else{
+//     $host = 'mysql';
+// }
+// define('DSN', 'mysql:dbname='.DB_NAME.';host='.print $host.';port=3306;charset=utf8');
+define('DSN', 'mysql:dbname='.DB_NAME.";host=mysql;port=3306;charset=utf8");  // データベースのDSN情報
